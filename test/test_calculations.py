cc84835da9efef0ff1552f82f8058f36715abf30
Jigar Patel <85085290+jigaraero@users.noreply.github.com>
2025-07-12 20:33:11 -0400

    Delete test directory

--- test/test_calculations.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from engine.performance import thrust
-
-class TestPerformance(unittest.TestCase):
-    def test_thrust(self):
-        self.assertAlmostEqual(thrust(10, 300), 3000)
-
-if __name__ == "__main__":
-    unittest.main()
